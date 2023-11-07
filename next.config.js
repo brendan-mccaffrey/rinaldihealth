0f73eefc1024e47dbb6202b3a5212494794a0028
brendan-mccaffrey <brendanchristophermccaffrey@gmail.com>
2023-11-07 12:49:46 -0500

    build config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+const repo = 'rinaldihealth'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     remotePatterns: [
       {
