bed39ad25d91ba072ced3bf3f345167c9be5e6fb
brendan-mccaffrey <brendanchristophermccaffrey@gmail.com>
2023-11-07 13:57:23 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ if (isGithubActions) {
   repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/rinaldi.health/`
-  basePath = `/`
+  basePath = ``
 }
 
 const nextConfig = {
